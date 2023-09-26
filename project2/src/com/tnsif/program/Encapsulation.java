0f4b00df37eebb8b1f5a66bc46b2052304ce20bd
acer <acer@10.0.0.120>
2023-09-26 14:39:10 +0530

    second commit

---
+++ project2/src/com/tnsif/program/Encapsulation.java
@@ -0,0 +1,13 @@
+package com.tnsif.program;
+
+public class Encapsulation {
+	//data members
+	public int serialno;
+	public String name;
+    public int age;
+    //member function
+    public void show( )
+    {
+    	System.out.println("serialno"+serialno+"\t"+"name"+"\t"+"age"+age+"\t");
+    }
+}
---
+++ project2/src/com/tnsif/program/EncpsulationDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.program;
+
+public class EncpsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation ob = new Encapsulation( );
+		ob.serialno=55;
+		ob.name="sowjanyaa";
+		ob.age=21;
+		ob.show( );
+
+	}
+
+}
