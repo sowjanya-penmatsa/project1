3708c55acc54a2621e33c8665e1edf123769cf42
acer <acer@10.0.0.111>
2023-10-09 09:36:31 +0530

    first commit

---
+++ project2/src/com/tns/overloading/OverloadDemo.java
@@ -0,0 +1,13 @@
+package com.tns.overloading;
+
+public class OverloadDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Point p1=new Point(6.8f);
+		System.out.println(p1);
+		Point p2=new Point(6.8f,9.8f);
+		System.out.println(p2);
+     }
+}
