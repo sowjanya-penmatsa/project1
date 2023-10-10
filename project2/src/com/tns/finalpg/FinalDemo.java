9fced87d51f4197c5086b5c95f08dd81c68f09de
acer <acer@TMP249-G2-M.bbrouter>
2023-10-10 15:30:44 +0530

    first commit

---
+++ project2/src/com/tns/finalpg/FinalDemo.java
@@ -0,0 +1,16 @@
+package com.tns.finalpg;
+
+public class FinalDemo {
+	int a=20;
+	void display() {
+		System.out.println("The value of a is"+a);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FinalDemo Fd=new FinalDemo();
+		Fd.display();
+
+	}
+
+}
